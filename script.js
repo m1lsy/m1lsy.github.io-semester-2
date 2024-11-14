e94256934a827b7448a5cf1477ebaed58f5bae85
m1lsy <521945@student.fontys.nl>
2024-11-14 13:32:59 +0100

    adding new quote section and start of slider

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Create the circle element
+// Create the circle cursor element
 const cursor = document.createElement('div');
 cursor.classList.add('circle-cursor');
 document.body.appendChild(cursor);
 
 // Update cursor position on mouse move
 document.addEventListener('mousemove', (e) => {
-  cursor.style.left = `${e.pageX}px`;
-  cursor.style.top = `${e.pageY}px`;
+  cursor.style.left = `${e.pageX}px`;  // Corrected template literal
+  cursor.style.top = `${e.pageY}px`;   // Corrected template literal
 });
